8250027fffd6379e891843a90513030953869fc3
yihonglei <yihonglei@hqzhuanche.com>
2021-02-17 18:03:18 +0800

    agent test

---
+++ sunflower-agent/src/test/java/com/jpeony/sunflower/agent/AgentStartupTest.java
@@ -0,0 +1,48 @@
+package com.jpeony.sunflower.agent;
+
+import com.jpeony.sunflower.agent.processor.ErrorMonitorProcessor;
+import com.jpeony.sunflower.agent.producer.MQProducer;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author yihonglei
+ */
+public class AgentStartupTest {
+    public static void main(String[] args) {
+        try {
+            MQProducer producer = MQProducer.getInstance();
+
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    producer.shutdown();
+                }
+            }));
+            /*
+             * Launch the producer instance.
+             */
+            producer.start();
+
+            CountDownLatch c = new CountDownLatch(1);
+            Thread errorThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        ErrorMonitorProcessor error = new ErrorMonitorProcessor();
+                        error.errorMonitor();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        c.countDown();
+                    }
+                }
+            });
+            errorThread.start();
+
+            c.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
